1e342179cde3d16820b4a0c1362c2afa23bbe620
Sovyan Klinin <ivanivanovich1234@gmail.com>
2025-04-12 15:32:21 +0300

    completed task

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,18 +1,21 @@
 <template>
     <div class="form">
         <div class="form__container">
-            <form class="form__items" action="/submit" method="POST" id="items">
+            <form class="form__items" action="/submit" method="POST" id="items" @submit.prevent="Submit">
 
-                <div class="form__item" v-for="item in items">
-                    <label for="{{ item.name }}" class="form__label">{{ item.title }}</label>
-                    <input v-if="item.type !== 'textarea'" type="{{ item.type }}" id="{{ item.id }}"
-                        name="{{ item.name }}" class="form__input" placeholder="{{ item.type }}" required>
-                    <textarea v-else type="{{ item.type }}" id="{{ item.id }}"
-                        name="{{ item.name }}" class="form__input" placeholder="{{ item.type }}" required></textarea>
+                <div class="form__item" v-for="item in items" :key="item.id">
+                    <label :for="item.name" class="form__label">{{ item.title }}</label>
+                    <input v-if="item.type !== 'textarea'" :type="item.type" :id="item.id" :name="item.name"
+                        v-model="formData[item.name]" class="form__input" :placeholder="item.placeholder">
+                    <textarea v-else :type="item.type" :id="item.id" :name="item.name" v-model="formData[item.name]"
+                        class="form__input" :placeholder="item.placeholder"></textarea>
                 </div>
 
                 <button type="submit" class="form__button">Отправить</button>
+
+                <p v-show="formError" class="form__error-message">Не все поля заполнены!</p>
             </form>
+
         </div>
     </div>
 </template>
@@ -45,8 +48,41 @@ export default {
                     title: 'Сообщение:',
                     placeholder: 'Введите ваше сообщение'
                 }
-            ]
+            ],
+
+            formData: {},
+
+            formError: false,
+
+        };
+    },
+
+    created(items) {
+        items = this.items;
+
+        items.forEach((item) => {
+            this.formData[item.name] = '';
+        });
+    },
+
+    methods: {
+
+        validateForm() {
+            this.formError = false;
+
+            for (const key in this.formData) {
+                if (!this.formData[key].trim()) {
+                    return this.formError = true;
+                }
+            }
+
+            return !this.formError;
+        },
+
+        Submit() {
+            this.validateForm();
+            console.log(this.formData);
         }
     }
-}
+};
 </script>
